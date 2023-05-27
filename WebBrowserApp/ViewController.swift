4ac76eb4f86bd44596c98766671dcc031238d754
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-05-28 00:12:01 +0300

    Creating a simple browser with WKWebView

--- WebBrowserApp/ViewController.swift
+++ WebBrowserApp/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+//Extends UIViewController, adheres to protocols of WKNavigationDelegate
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        //instance of WKWebView class
+        webView = WKWebView()
+        //Delegation - a programming pattern
+        // A delegate is one thing acting in place of another, effectively answering questions and responding to actions on its behalf
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //type URL - swift's way of storing location of files
+        let url = URL(string:"https://github.com/leonardsangoroh")!
+        //Wraps urls in a url request
+        webView.load(URLRequest(url:url))
+        //Enables a property of the webview - BackForward Navigation
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
