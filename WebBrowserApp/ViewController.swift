e89209d9f554c9aca0ac2fb9cc9c35b722000411
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-10-19 00:31:45 +0300

    Web page loaded to application - done

--- WebBrowserApp/ViewController.swift
+++ WebBrowserApp/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-//Extends UIViewController, adheres to protocols of WKNavigationDelegate
+//Extends UIViewController, adheres or conforms to protocols of WKNavigationDelegate
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
@@ -19,16 +19,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
         //Delegation - a programming pattern
         // A delegate is one thing acting in place of another, effectively answering questions and responding to actions on its behalf
         webView.navigationDelegate = self
+        //make the view the view for the view controller
         view = webView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+//
         //type URL - swift's way of storing location of files
         let url = URL(string:"https://github.com/leonardsangoroh")!
         //Wraps urls in a url request
         webView.load(URLRequest(url:url))
         //Enables a property of the webview - BackForward Navigation
+        //Swipe left or right to move backwards or forwards
         webView.allowsBackForwardNavigationGestures = true
     }
 
